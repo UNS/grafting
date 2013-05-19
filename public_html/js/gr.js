1d74028f6b12783ced544b8a5daea080f6bfe0a4
remal <remal@DNS>
2013-05-19 19:20:19 +0400

    hi

--- public_html/js/gr.js
+++ public_html/js/gr.js
@@ -10,45 +10,71 @@ var root = this;
 		this.y = y;
 		this.r = 50;
 		this.t = 0;
+		this.u = 90;
 		this.alpha = Math.PI * Math.random();
+		this.phase = 1;
 		console.log("create " + JSON.stringify(this));
 	};
 
-	var draw = function(p, diff, alpha) {
+	var draw = function(p, dis, dir, u, r) {
+		if (p.phase !== 1) {
+			var x1 = r * Math.cos(u * Math.PI / 180);
+			var y1 = r * Math.sin((360 - u) * Math.PI / 180);
+			var x2 = x1;
+			var y2 = r * Math.sin(u * Math.PI / 180);
+			var r2 = r * Math.sqrt(1 + Math.pow(1 / Math.tan((90 - u) * Math.PI / 180), 2));
+			var tt = r2 - dis;
+			var y3 = 0;
+			console.log(" p " + p + " dis " + dis + " dir " + dir + " u " + u + " r " + r);
+
+			if (tt <= 0) {
+				p.phase = 3;
+			}
+			var y3 = Math.tan((90 - u) * Math.PI / 180) * tt;
+		}
+		ctx.save();
+		ctx.beginPath();
 		ctx.translate(p.x, p.y);
-		ctx.rotate(alpha);
-		ctx.fillStyle = 'gray';
-		ctx.beginPath();
-		ctx.arc(0 - diff, 0, p.r, Math.PI / 2, 3 * Math.PI / 2);
+		if (p.phase === 2) {
+			ctx.moveTo(0, -y3);
+			ctx.lineTo(0, y3);
+		} else if (p.phase === 3) {
+			ctx.moveTo(dis - r2, 0);
+		}
+		ctx.rotate(dir + Math.PI);
+		if (p.phase === 1) {
+			ctx.arc(0 - dis, 0, r,
+			Math.PI/2,
+			3 * Math.PI / 2,
+			false);
+		} else {
+		ctx.arc(0 - dis, 0, r,
+			u * Math.PI / 180,
+			(360 - u) * Math.PI / 180,
+			false);
+		}
+		ctx.rotate(Math.PI);
+		if (p.phase === 2) {
+			ctx.moveTo(0, -y3);
+			ctx.lineTo(0, y3);
+		} else if (p.phase === 3) {
+			ctx.moveTo(dis - r2, 0);
+		}
+		if (p.phase === 1) {
+			ctx.arc(0 - dis, 0, r,
+			Math.PI/2,
+			3 * Math.PI / 2,
+			false);
+		} else {
+		ctx.arc(0 - dis, 0, r,
+			u * Math.PI / 180,
+			(360 - u) * Math.PI / 180,
+			false);
+		}
 		ctx.closePath();
+		ctx.fillStyle = "gray";
 		ctx.fill();
-//		ctx.moveTo(p.x - diff, p.y - p.r);
-//		ctx.lineTo(p.x + diff, p.y - p.r);
-		ctx.fillRect(0 - diff, 0 - p.r, 2 * diff, 2 * p.r);
-		ctx.beginPath();
-		ctx.arc(0 + diff, 0, p.r, 3 * Math.PI / 2, 5 * Math.PI / 2);
-		ctx.closePath();
-		ctx.fill();
-//		ctx.moveTo(p.x + diff, p.y + p.r);	
-//		ctx.lineTo(p.x - diff, p.y + p.r);
-		ctx.fill();
-//		ctx.fillStyle = "black";
-//		ctx.fillRect(0, 0, canvas.width, canvas.height);
-//		ctx.translate(10, 100);
-//		ctx.beginPath();
-//		ctx.moveTo(p1.x, p1.y);
-//		ctx.lineTo(p2.x, p2.y);
-//		ctx.lineTo(p3.x, p3.y);
-//		ctx.closePath();
-//		ctx.fillStyle = 'gray';
-//		ctx.fill();
-//		ctx.strokeStyle = 'white';
-//		ctx.stroke();
-//		ctx.restore();
-//		ctx.lineTo(p2.x, p2.y);
-//		ctx.lineTo(p4.x, p4.y);
-//		ctx.stroke();
-//		ctx.font = 'italic 20pt Calibri';
+		ctx.restore();
 	};
 
 	var p = new point(500, 200);
@@ -67,6 +93,7 @@ var root = this;
 					jmin = j;
 				}
 			}
+
 			if (min > 0) {
 				a[jmin].s = true;
 				a[jmin].dx = 0;
@@ -84,8 +111,26 @@ var root = this;
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 		for (var i = 0; i < a.length; i++) {
 			a[i].t += 0.3;
+			if (a[i].t > 10)
+				a[i].phase = 2;
 			ctx.save();
-			draw(a[i], a[i].t - a[i].t % 1, a[i].alpha);
+			switch (a[i].phase) {
+				case 1:
+					console.log("phase 1");
+					draw(a[i], a[i].t - a[i].t % 1, a[i].alpha, a[i].u, a[i].r);
+					break;
+
+				case 2:
+					a[i].u += 1;
+					draw(a[i], a[i].t - a[i].t % 1, a[i].alpha, a[i].u, a[i].r);
+					break;
+
+				case 3:
+					a[i].u += 3;
+					draw(a[i], a[i].t - a[i].t % 1, a[i].alpha, a[i].u, a[i].r);
+					break;
+			}
+			draw(a[i], a[i].t - a[i].t % 1, a[i].alpha, a[i].u);
 			ctx.restore();
 		}
 	};
